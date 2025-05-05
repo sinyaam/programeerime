6fe7d31e8217db7834ec54d1db957b7c50ebec92
sinyaam <novagg43@gmail.com>
2025-05-05 08:47:56 +0300

    Add files via upload

--- Roga/game_map.py
+++ Roga/game_map.py
@@ -1,7 +1,10 @@
 from tile import Tile
 from rectangle import Rect
 from random import randint
-
+from entity import Entity
+import tcod as libtcod
+from components.ai import BasicMonster
+from components.fighter import Fighter
 class GameMap:
     def __init__(self, width, height):
         self.width = width
@@ -14,7 +17,9 @@ class GameMap:
         
 
         return tiles
-    def make_map(self, max_rooms, room_min_size, room_max_size, map_width, map_height, player):
+    def make_map(self, max_rooms, room_min_size, room_max_size, map_width, map_height, player,entities,max_monsters_per_room):
+        rooms = []
+        num_rooms = 0
         for r in range(max_rooms):
             # random width and height
             w = randint(room_min_size, room_max_size)
@@ -22,6 +27,39 @@ class GameMap:
             # random position without going out of the boundaries of the map
             x = randint(0, map_width - w - 1)
             y = randint(0, map_height - h - 1)
+            new_room = Rect(x, y, w, h)
+
+           # run through the other rooms and see if they intersect with this one
+            for other_room in rooms:
+                if new_room.intersect(other_room):
+                    break
+            else:
+                # this means there are no intersections, so this room is valid
+ 
+                # "paint" it to the map's tiles
+                self.create_room(new_room)
+ 
+                # center coordinates of new room, will be useful later
+                (new_x, new_y) = new_room.center()
+ 
+                if num_rooms == 0:
+                    # this is the first room, where the player starts at
+                    player.x = new_x
+                    player.y = new_y
+                else:
+                    (prev_x, prev_y) = rooms[num_rooms - 1].center()
+                    if randint(0,1) == 1:
+                        self.create_h_tunnel(prev_x, new_x, prev_y)
+                        self.create_v_tunnel(prev_y, new_y, new_x)
+                    else:
+                        # first move vertically, then horizontally
+                        self.create_v_tunnel(prev_y, new_y, prev_x)
+                        self.create_h_tunnel(prev_x, new_x, new_y)
+ 
+                # finally, append the new room to the list
+                self.place_entities(new_room, entities, max_monsters_per_room)
+                rooms.append(new_room)
+                num_rooms += 1
         
     def create_room(self, room):
        # go through the tiles in the rectangle and make them passable
@@ -38,6 +76,23 @@ class GameMap:
         for y in range(min(y1, y2), max(y1, y2) + 1):
             self.tiles[x][y].blocked = False
             self.tiles[x][y].block_sight = False
+    def place_entities(self , room , entities , max_monsters_per_room):
+        number_of_monsters = randint(0, max_monsters_per_room)
+            
+        for i in range(number_of_monsters):
+            x = randint(room.x1 + 1, room.x2 -1)
+            y = randint(room.y1 + 1, room.y2 -1)
+            if not any([entity for entity in entities if entity.x == x and entity.y == y]):
+                if randint(0 , 100) < 80:
+                    fighter_component = Fighter(hp=10, defense=0, power=3)
+                    ai_component = BasicMonster()
+                    monster = Entity(x,y,'o' , libtcod.desaturated_green, "Orc" , blocks=True, fighter=fighter_component, ai=ai_component)
+                else:
+                    fighter_component = Fighter(hp=16, defense=1, power=4)
+                    ai_component = BasicMonster()
+                    monster = Entity(x,y,'T' , libtcod.darker_green, "Troll", blocks=True, fighter=fighter_component, ai=ai_component)
+                    
+            entities.append(monster)
     def is_blocked(self, x, y):
         if self.tiles[x][y].blocked:
             return True
--- Roga/input_handlers.py
+++ Roga/input_handlers.py
@@ -3,14 +3,23 @@ import libtcodpy as libtcod
 
 def handle_keys(key):
     # Movement keys
-    if key.vk == libtcod.KEY_UP:
+    key_char = chr(key.c)
+    if key_char == 'w':
         return {'move': (0, -1)}
-    elif key.vk == libtcod.KEY_DOWN:
+    elif key_char == 's':
         return {'move': (0, 1)}
-    elif key.vk == libtcod.KEY_LEFT:
+    elif key_char == 'a':
         return {'move': (-1, 0)}
-    elif key.vk == libtcod.KEY_RIGHT:
+    elif key_char == 'd':
         return {'move': (1, 0)}
+    elif key_char == 'q':
+        return {'move': (-1, -1)}
+    elif key_char == 'e':
+        return {'move': (1, -1)}
+    elif key_char == 'c':
+        return {'move': (1, 1)}
+    elif key_char == 'z':
+        return {'move': (-1, 1)}
 
     if key.vk == libtcod.KEY_ENTER and key.lalt:
         # Alt+Enter: toggle full screen
@@ -18,6 +27,7 @@ def handle_keys(key):
     elif key.vk == libtcod.KEY_ESCAPE:
         # Exit the game
         return {'exit': True}
+    
 
     # No key was pressed
     return {}
